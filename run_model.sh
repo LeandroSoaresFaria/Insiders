dba54144692acc29e5c1c01a5854b32303025dc0
LeandroSoaresFaria <leandro_faria@yahoo.com.br>
2021-11-07 20:18:55 -0300

    Initial update

---
+++ run_model.sh
@@ -0,0 +1,9 @@
+# Variable
+data=$(date +'%Y-%m-%dT%H:%M:%S')
+
+# path
+path='/home/leandro/repos/insiders_clustering'
+path_to_envs='/home/leandro/.pyenv/versions/pa005insiderscluestering/bin'
+
+
+$path_to_envs/papermill $path/src/models/C11-lsf-Deploy.ipynb $path/reports/C11-lsf-Deploy_$data.ipynb
