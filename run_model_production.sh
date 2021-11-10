4412cd494297986c879124f40ed1943541dbf2ec
LeandroSoaresFaria <leandro_faria@yahoo.com.br>
2021-11-10 10:56:15 -0300

    test contrab

--- run_model_production.sh
+++ run_model_production.sh
@@ -3,7 +3,7 @@ data=$(date +'%Y-%m-%dT%H:%M:%S')
 
 # path
 path='/home/ubunto/insiders_clustering'
-path_to_envs='/home/ubunto/.pyenv/versions/pa005insiderscluestering/bin'
+path_to_envs='/home/ubuntu/.local/bin'
 
 
 $path_to_envs/papermill $path/src/models/C11-lsf-Deploy.ipynb $path/reports/C11-lsf-Deploy_$data.ipynb
